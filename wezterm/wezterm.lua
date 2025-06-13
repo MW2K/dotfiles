b7968ba9fee3d5fdd606722ef566c8ac34058bef
Peter Greenwell <48398448+MW2K@users.noreply.github.com>
2025-06-14 01:42:39 +1000

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,15 +7,15 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 config.font = wezterm.font 'FiraCode Nerd Font Mono'
-config.initial_cols = 120
-config.initial_rows = 35
+config.initial_cols = 100
+config.initial_rows = 50
 config.enable_tab_bar = false
 config.window_background_opacity = 0.9
 
 
 
 -- For example, changing the color scheme:
-config.color_scheme = 'GruvboxLight'
+config.color_scheme = 'Catppuccin Latte'
 
 -- and finally, return the configuration to wezterm
 return config
